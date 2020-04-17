a1c0f83ee30ef2d7a10ec04f5efc8bd283a0f8f5
sttagami2 <sttagami2@gmail.com>
2020-04-17 09:37:49 +0000

    [Add] Item, Typeの初期値を設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :cart_items, dependent: :destroy
-  belongs_to :types
+  belongs_to :types, optional: true
 
   enum status: { 販売中: 0, 売切れ: 1 }
 end
