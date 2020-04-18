fbb0eeeba292ace94ce7dce847fa226220b2e0c6
Naoki1126 <m.naoki85@gmail.com>
2020-04-18 12:06:24 +0000

    odersテーブルをordersテーブルに変更するマイグレーションファイルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_163942) do
+ActiveRecord::Schema.define(version: 2020_04_18_120306) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -63,7 +63,17 @@ ActiveRecord::Schema.define(version: 2020_04_16_163942) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "oders", force: :cascade do |t|
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.integer "status", default: 0, null: false
+    t.integer "quontity", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.integer "shipping_cost", default: 800, null: false
     t.integer "payment", null: false
@@ -72,16 +82,6 @@ ActiveRecord::Schema.define(version: 2020_04_16_163942) do
     t.text "address", null: false
     t.string "destination", null: false
     t.integer "invoice", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "order_items", force: :cascade do |t|
-    t.integer "order_id", null: false
-    t.integer "item_id", null: false
-    t.integer "status", default: 0, null: false
-    t.integer "quontity", null: false
-    t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
