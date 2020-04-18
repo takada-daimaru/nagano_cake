143ecb48e7ba6dab12566104b8629fbf65f999fe
sttagami2 <sttagami2@gmail.com>
2020-04-18 15:52:25 +0000

    [Update] OrderのViewとStyleを作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0ed4cad6e0aefe7516bd5b5690dc7e416e90a230c1f38a439602fa598e9af5984c0268819a3ac0929d50700ab2a9b775c7d73b06674a32cb27666426980fcb91'
