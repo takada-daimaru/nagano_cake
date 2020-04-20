6a73839c42bf739d316c1932716c6553d56a8166
Naoki1126 <m.naoki85@gmail.com>
2020-04-20 05:21:06 +0000

    routeにendを追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Rails.application.routes.draw do
 
     resources :cart_items, only: [:index, :create, :update, :destroy] 
      resources :items, only: [:new, :create]
+  end
 
   resources :items, only: [:index, :show ]
   resources :orders, only: [:new, :create, :index, :show]
