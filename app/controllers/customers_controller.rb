a3ea2eb1bbec72cec5487eaefee4857936696290
Naoki1126 <m.naoki85@gmail.com>
2020-04-22 13:41:13 +0000

    めちゃくちゃいじりました。

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,19 +8,38 @@ class CustomersController < ApplicationController
     
   end
 
-  def update
+  def passwords
+    @customer = current_customer
+  end
 
+  def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_path
+
+  end
+
+  def passwordsupdate
+    @customer = current_customer
+    @customer.update(password_params)
+    redirect_to root_path
   end
 
   def status
     
   end
 
-  def update_status
-    
+  def update_
   end
 
-  private
+private
+def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email ,:postcode, :address, :phone_number )
+end
+
+def password_params
+    params.require(:customer).permit(:password, :password_confirmation,:current_password )
+end
 
   
 end
